27faebfbfb61d36eb8568e7a3ff875224b866a34
traderforever <smi20050@byui.edu>
2020-11-07 12:21:24 -0500

    updatedwekk8

--- lesson8/js/lastModified.js
+++ lesson8/js/lastModified.js
@@ -1,1 +1,7 @@
 document.getElementById("lastUpdated").innerHTML = `Last Updated :   ${document.lastModified}`;
+
+
+
+function adjustRating(severity) {
+    document.getElementById("severity").innerHTML = severity;
+}
