08957b84eca632c14e4b6130eada8f008726acc4
traderforever <smi20050@byui.edu>
2020-11-14 21:25:08 -0500

    endofweek9

--- lesson9/js/scripts.js
+++ lesson9/js/scripts.js
@@ -19,3 +19,51 @@ if (currDay == 5) {
         pancake.item(i).style.display = "block";
     }
 }
+
+/* WEEK 9 JSON */
+const townDataUrl = "https://byui-cit230.github.io/weather/data/towndata.json";
+
+fetch(townDataUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  
+  .then(function (jsonObject) {
+    const towns = jsonObject['towns'];
+    for (let i = 0 ; i < towns.length -1; i++) {
+            if (i == 0) {
+                continue;
+            }
+            else if (i == 2) {
+                continue;
+            }
+            else if (i ==3) {
+                continue;
+            }
+
+        let card = document.createElement('section');
+        let townName = document.createElement('h2');
+        let townMotto = document.createElement('div');
+        let founded = document.createElement('p');
+        let currPop = document.createElement('p');
+        let avgRain = document.createElement('p');
+        let image = document.createElement('img');
+
+        townName.textContent = towns[i].name;
+        townMotto.textContent = towns[i].motto;
+        founded.textContent = "Year founded: " + towns[i].yearFounded;
+        currPop.textContent = "Population: " + towns[i].currentPopulation;
+        avgRain.textContent = "Annual Rainfall: " + towns[i].averageRainfall;
+        image.setAttribute('src', "images/" + towns[i].photo);
+        image.setAttribute('alt', townName.textContent);
+
+        card.appendChild(townName);
+        card.appendChild(townMotto);
+        card.appendChild(founded);
+        card.appendChild(currPop);
+        card.appendChild(avgRain);
+        card.appendChild(image);
+
+        document.querySelector('.wk9card').appendChild(card);
+    }
+});
