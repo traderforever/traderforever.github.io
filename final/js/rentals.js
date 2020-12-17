0582c608df0313cf76fe3462e18100410d727e78
traderforever <smi20050@byui.edu>
2020-12-16 21:15:25 -0500

    json update

--- final/js/rentals.js
+++ final/js/rentals.js
@@ -24,10 +24,45 @@ fetch(requestURL)
             img.alt = rentals[i].name;
 
             card.appendChild(h2);
+            card.appendChild(img);
             card.appendChild(par1);
-            card.appendChild(par2);
-            card.appendChild(img)
 
             document.querySelector('div.rentaltypes').appendChild(card);
+        }
+    });
+
+    fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject);
+        const rentals = jsonObject['rentals'];
+        for (let i = 0; i < rentals.length; i++ ) {
+            let card = document.createElement('tr');
+            let th = document.createElement('th');
+            let th1 = document.createElement('th');
+            let th2 = document.createElement('th');
+            let th3 = document.createElement('th');
+            let th4 = document.createElement('th');
+            // let thclass = document.createElement('th')
+
+
+            th.textContent = rentals[i].vehicle ;
+            th1.textContent = "Reservation: " + rentals[i].resHalfday;
+            th2.textContent = "Reservation: " + rentals[i].resFullday;
+            th3.textContent = "Walk-in: " +  rentals[i].halfday;
+            th4.textContent = "Walk-in: " +  rentals[i].fullday;
+            // thclass.className = 
+
+
+            card.appendChild(th);
+            card.appendChild(th1);
+            card.appendChild(th2);
+            card.appendChild(th3);
+            card.appendChild(th4);
+
+
+            document.querySelector('div.rentaltable').appendChild(card);
         }
     });
